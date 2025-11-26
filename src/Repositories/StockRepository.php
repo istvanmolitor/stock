c18308335b632e1655eeffa17746bcae98e90a1f
Molitor István <istvan.molitor@centralmediacsoport.hu>
2025-11-26 20:21:50 +0100

    Stock repository

--- src/Repositories/StockRepository.php
+++ src/Repositories/StockRepository.php
@@ -2,7 +2,9 @@
 
 namespace Molitor\Stock\Repositories;
 
+use Molitor\Product\Models\Product;
 use Molitor\Stock\Models\Stock;
+use Molitor\Stock\Models\WarehouseRegion;
 
 class StockRepository implements StockRepositoryInterface
 {
@@ -11,5 +13,47 @@ class StockRepository implements StockRepositoryInterface
     public function __construct()
     {
         $this->stock = new Stock();
+    }
+
+    public function getQuantity(WarehouseRegion $warehouseRegion, Product $product): int
+    {
+        return (int)$this->stock->where('warehouse_region_id', $warehouseRegion->id)
+            ->where('product_id', $product->id)
+            ->value('quantity');
+    }
+
+    public function exists(WarehouseRegion $warehouseRegion, Product $product): bool
+    {
+        return (int)$this->stock->where('warehouse_region_id', $warehouseRegion->id)
+            ->where('product_id', $product->id)
+            ->exists();
+    }
+
+    public function setQuantity(WarehouseRegion $warehouseRegion, Product $product, int $quantity): void
+    {
+        if($quantity <= 0) {
+            $this->delete($warehouseRegion, $product);
+        }
+        elseif($this->exists($warehouseRegion, $product)) {
+            $this->stock->where('warehouse_region_id', $warehouseRegion->id)
+                ->where('product_id', $product->id)
+                ->update([
+                    'quantity' => $quantity,
+                ]);
+        }
+        else {
+            $this->stock->create([
+                'warehouse_region_id' => $warehouseRegion->id,
+                'product_id' => $product->id,
+                'quantity' => $quantity,
+            ]);
+        }
+    }
+
+    public function delete(WarehouseRegion $warehouseRegion, Product $product): void
+    {
+        $this->stock->where('warehouse_region_id', $warehouseRegion->id)
+            ->where('product_id', $product->id)
+            ->delete();
     }
 }
--- src/Repositories/StockRepositoryInterface.php
+++ src/Repositories/StockRepositoryInterface.php
@@ -2,7 +2,16 @@
 
 namespace Molitor\Stock\Repositories;
 
+use Molitor\Product\Models\Product;
+use Molitor\Stock\Models\WarehouseRegion;
+
 interface StockRepositoryInterface
 {
-    
+    public function getQuantity(WarehouseRegion $warehouseRegion, Product $product): int|null;
+
+    public function exists(WarehouseRegion $warehouseRegion, Product $product): bool;
+
+    public function setQuantity(WarehouseRegion $warehouseRegion, Product $product, int $quantity): void;
+
+    public function delete(WarehouseRegion $warehouseRegion, Product $product): void;
 }
