0d4619f3fe7978cb031327d825ac3dc8130f8039
Molitor István <istvan.molitor@centralmediacsoport.hu>
2025-11-26 21:37:32 +0100

    StockService

--- src/Services/StockService.php
+++ src/Services/StockService.php
@@ -3,51 +3,116 @@
 namespace Molitor\Stock\Services;
 
 use Molitor\Stock\Enums\StockMovementType;
-use Molitor\Stock\Models\Stock;
 use Molitor\Stock\Models\StockMovement;
 use Molitor\Stock\Models\StockMovementItem;
+use Molitor\Stock\Repositories\StockRepositoryInterface;
 
 class StockService
 {
     public function __construct(
-        protected StockReposi
+        protected StockRepositoryInterface $stockRepository,
     )
     {
-
     }
 
-    public function execute(StockMovement $stockMovement): void
+    public function execute(StockMovement $stockMovement): array
     {
         if ($stockMovement->type === StockMovementType::In) {
-            $this->incrise($stockMovement);
+            $this->increase($stockMovement);
+            return [];
+        }
+        elseif ($stockMovement->type === StockMovementType::Out) {
+            return $this->decrease($stockMovement);
+        }
+        elseif ($stockMovement->type === StockMovementType::Transfer) {
+            return $this->transfer($stockMovement);
         }
     }
 
-    public function incrise(StockMovement $stockMovement): void
+    private function increase(StockMovement $stockMovement): void
     {
-        $stockMovement->load('stockMovementItems');
-
         /** @var StockMovementItem $item */
-        foreach ($stockMovement->stockMovementItems as $item) {
-            if (empty($item->warehouse_region_id) || empty($item->product_id)) {
-                continue;
-            }
+        foreach ($stockMovement->stockMovementItems as $stockMovementItem) {
+            $product = $stockMovementItem->product;
+            $warehouseRegion = $stockMovementItem->warehouseRegion;
+            $currentQuantity = $this->stockRepository->getQuantity($warehouseRegion, $product);
+            $this->stockRepository->setQuantity(
+                $warehouseRegion,
+                $product,
+                $currentQuantity + $stockMovementItem->quantity
+            );
+        }
+    }
 
-            $stock = Stock::where('warehouse_region_id', $item->warehouse_region_id)
-                ->where('product_id', $item->product_id)
-                ->lockForUpdate()
-                ->first();
+    private function validateDecrease(StockMovement $stockMovement): array
+    {
+        foreach ($stockMovement->stockMovementItems as $stockMovementItem) {
+            $product = $stockMovementItem->product;
+            $warehouseRegion = $stockMovementItem->warehouseRegion;
 
-            if ($stock) {
-                $stock->quantity = (float) $stock->quantity + (float) $item->quantity;
-                $stock->save();
-            } else {
-                Stock::create([
-                    'warehouse_region_id' => $item->warehouse_region_id,
-                    'product_id' => $item->product_id,
-                    'quantity' => $item->quantity,
-                ]);
+            $currentQuantity = $this->stockRepository->getQuantity($warehouseRegion, $product);
+            $newQuantity = $currentQuantity - $stockMovementItem->quantity;
+
+            if($newQuantity < 0) {
+                $errors[] = [
+                    'product' => $product,
+                    'region' => $warehouseRegion,
+                    'quantity' => $stockMovementItem->quantity,
+                    'currentQuantity' => $currentQuantity,
+                ];
             }
         }
+
+        return $errors;
+    }
+
+    private function decrease(StockMovement $stockMovement): array
+    {
+        $errors = $this->validateDecrease($stockMovement);
+        if(count($errors)) {
+            return $errors;
+        }
+
+        /** @var StockMovementItem $item */
+        foreach ($stockMovement->stockMovementItems as $stockMovementItem) {
+            $product = $stockMovementItem->product;
+            $warehouseRegion = $stockMovementItem->warehouseRegion;
+
+            $currentQuantity = $this->stockRepository->getQuantity($warehouseRegion, $product);
+            $this->stockRepository->setQuantity(
+                $warehouseRegion,
+                $product,
+                $currentQuantity - $stockMovementItem->quantity
+            );
+        }
+        return [];
+    }
+
+    private function transfer(StockMovement $stockMovement): array
+    {
+        $errors = $this->decrease($stockMovement);
+        if(count($errors)) {
+            return $errors;
+        }
+
+        $newStockMovement = new StockMovement([
+            'type' => StockMovementType::In,
+            'warehouse_id' => $stockMovement->warehouse_id,
+            'description' => $stockMovement->description,
+            'linked_stock_movement_id' => $stockMovement->id,
+        ]);
+        $newStockMovement->save();
+
+        /** @var StockMovementItem $stockMovementItem */
+        foreach ($newStockMovement->stockMovementItems as $stockMovementItem) {
+            $newStockMovementItem = new StockMovementItem([
+                'stock_movement_id' => $newStockMovement->id,
+                'product_id' => $stockMovementItem->product_id,
+                'quantity' => $stockMovementItem->quantity,
+                'warehouse_region_id' => null,
+            ]);
+            $newStockMovementItem->save();
+        }
+        return [];
     }
 }
